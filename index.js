d5f6a4928eff14470361572c4312e64428ee904d
Alejandro Larios <alexlarios70@hotmail.com>
2023-10-23 17:39:06 -0400

    Adding the index and inquirer functionality

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const inquirer = require ('inquirer');
 const mysql = require ('mysql2') ;
+
+const init = () => {
+    inquirer.prompt ([
+        {
+        type: 'list',
+        name: 'selection',
+        choices: [
+            'View all departments', 'View all roles', 'View all employees', 'add a department', 'Add a role', 'Update an employee role','Quit'
+        ]
+        }
+    ])
+    .then((data) =>{
+        switch(`${data.selection}`){
+            case 'View all departments':
+        }
+    })
+}
