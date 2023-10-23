ef12d203ee467983028f8110876339665a08d389
Alejandro Larios <alexlarios70@hotmail.com>
2023-10-23 19:17:03 -0400

    Database working

--- db/schema.sql
+++ db/schema.sql
@@ -5,28 +5,29 @@ USE employeeInsects_db;
 
 CREATE TABLE department(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-department_name VARCHAR (30) NOT NULL
-)
+department_name VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE roles (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30),
 salary DECIMAL,
-department_id INT
-FOREIGN KEY (department_id)
+department_id INT,
+FOREIGN KEY(department_id)
 REFERENCES department(id)
 ON DELETE SET NULL
-)
+);
 
-CREATE TABLE employees (
+CREATE TABLE employees(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
-role_id INT
-manager_id INT
-FOREIGN KEY (role_id)
-REFERENCES roles (id)
+role_id INT,
+manager_id INT,
+FOREIGN KEY(role_id)
+REFERENCES roles(id)
 ON DELETE SET NULL,
-FOREIGN KEY (manager_id)
-REFERENCES employee (id)
-)
+FOREIGN KEY(manager_id)
+REFERENCES employees(id)
+ON DELETE SET NULL
+);
